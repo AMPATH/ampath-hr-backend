29870cea7cbc868aa8df17cfecd5cca3dc9cdf96
Eric Kibet <83347293+Eriq-Kibet@users.noreply.github.com>
2021-09-29 14:51:31 +0300

    Hr 005  added upload function (#7)
    * Added More Services
    
    * Added more Services
    
    * added upload function

--- src/services/timesheet.service.ts
+++ src/services/timesheet.service.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import serviceDef from '../connection/connection';
 import { TimesheetsUpdate } from '../types/employee';
 
@@ -14,14 +15,37 @@ export function GetTimesheets(pfNumber): Promise<any> {
 
 export function AddTimesheets(timesheet: TimesheetsUpdate) {
   const { pfNumber, month, upload } = timesheet;
-  // eslint-disable-next-line max-len
-  const sql = `INSERT INTO Timesheets (pfNumber, month, upload) VALUES ('${pfNumber}', '${month}', '${upload}')`;
-  return new Promise((resolve, reject) => {
-    serviceDef.dbConnection().then((pool: any) => {
-      pool.query(sql, (error: any, results: any, fields: any) => {
-        if (error) reject(error);
-        resolve(results);
-      });
+  const name = upload.hapi.filename;
+  const employees = JSON.parse(pfNumber);
+  if (upload) {
+    const fileName = upload.hapi.filename;
+    const path = `${__dirname}/../../uploads/${fileName}`;
+    const file = fs.createWriteStream(path);
+    file.on('error', (error) => {
+      if (error) throw error
     });
+    upload.pipe(file);
+    upload.on('end', (error) => {
+      if (error) throw error
+      const uploadDetails = {
+        filename: upload.hapi.filename,
+        headers: upload.hapi.headers,
+      };
+      return JSON.stringify(uploadDetails);
+    });
+  }
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < employees.length; i++) {
+      serviceDef.dbConnection().then((pool: any) => {
+        pool.query(`
+          INSERT INTO Timesheets(pfNumber, month, upload) VALUES(${employees[i]}, '${month}', '${name}')`,
+          (error: any, results: any, fields: any) => {
+            if (error) reject(error);
+            resolve(results);
+          },
+        );
+      });
+    }
   });
 }
