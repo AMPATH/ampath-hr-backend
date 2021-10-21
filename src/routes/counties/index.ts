d48259f18ff22e3ddd82cc2f3ebfe301b6f52669
Eric Kibet <83347293+Eriq-Kibet@users.noreply.github.com>
2021-10-21 10:19:28 +0300

    Hr 009, Added services for Counties (#14)
    * Added More Services
    
    * Added more Services
    
    * Added more services for budget
    
    * Added more services for Counties

--- src/routes/counties/index.ts
+++ src/routes/counties/index.ts
@@ -1,7 +1,7 @@
 import countyController from '../../controllers/county.controller';
 import { Route } from '../../config/interfaces';
 
-const route: Route = {
+const route: Array<Route> = [{
   path: '/county',
   method: 'GET',
   handler: countyController,
@@ -10,5 +10,37 @@ const route: Route = {
     description: 'get, all counties that have been captured in the system',
     notes: 'Returns an array of counties',
   },
-};
+},
+{
+  path: '/county',
+  method: 'POST',
+  handler: countyController,
+  options: {
+    tags: ['api'],
+    description: 'post, Inserts counties that have been captured in the system',
+    notes: `Request body, an example below
+
+            {
+                "name": "xyz",
+                "countyId": 6
+            }
+            `,
+  },
+}, {
+  path: '/county',
+  method: 'PUT',
+  handler: countyController,
+  options: {
+    tags: ['api'],
+    description: 'put, update counties that have been captured in the system using the county id',
+    notes: `Request body, an example below',
+          {
+              "name": "xyz",
+              "countyId": 6
+          }
+          `,
+
+  },
+}
+];
 export default route;
