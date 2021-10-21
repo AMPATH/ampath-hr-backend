f1fed86da96b6a92fe8c97c0ce62337f4cdce47b
Eric Kibet <83347293+Eriq-Kibet@users.noreply.github.com>
2021-10-21 08:31:11 +0300

    Hr 008 Added more services for Budget (#13)
    * Added more services for budget

--- src/controllers/budget.controller.ts
+++ src/controllers/budget.controller.ts
@@ -1,9 +1,25 @@
 import { Request, ResponseToolkit } from '@hapi/hapi';
 import response from '../utils/response';
-import Budgets from '../services/budget.service';
+import { AddBudgets, GetBudgets, UpdateBudget } from '../services/budget.service';
+import { BudgetDetails, UpdateBudgetDetails } from '../types/employee';
 
 const budgetController = async (request: Request, h: ResponseToolkit): Promise<any> => {
-  const budget = await Budgets().then((results) => results);
-  return h.response(response(200, budget));
+  switch (request.method) {
+    case 'get': {
+      const budget: any = await GetBudgets().then((results) => results);
+      return h.response(response(!budget.length ? 500 : 200, budget)).code(!budget.length ? 500 : 200);
+    }
+    case 'post': {
+      const budget: any = await AddBudgets(request.payload as BudgetDetails).then((results) => results);
+      return h.response(response(!budget.length ? 500 : 200, budget)).code(!budget.length ? 500 : 200);
+    }
+    case 'put': {
+      const budget: any = await UpdateBudget(request.payload as UpdateBudgetDetails).then((results)=> results );
+      return h.response(response(!budget.length ? 500 : 200, budget)).code(!budget.length ? 500 : 200);
+    }
+    default:
+      return h.response(response(404, 'Not found'));
+  }
+
 };
 export default budgetController;
