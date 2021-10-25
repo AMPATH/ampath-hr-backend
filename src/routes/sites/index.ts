04613890d79be759751c3e26195ba87527530eee
Eric <eriqthedev@gmail.com>
2021-10-25 11:27:24 +0300

    Added Services for Sites

--- src/routes/sites/index.ts
+++ src/routes/sites/index.ts
@@ -1,7 +1,7 @@
 import siteController from '../../controllers/site.controller';
 import { Route } from '../../config/interfaces';
 
-const route: Route = {
+const route: Array<Route> = [{
   path: '/site',
   method: 'GET',
   handler: siteController,
@@ -10,5 +10,37 @@ const route: Route = {
     description: 'get, all sites that have been captured in the system',
     notes: 'Returns an array of sites',
   },
-};
+},
+{
+  path: '/site',
+  method: 'POST',
+  handler: siteController,
+  options: {
+    tags: ['api'],
+    description: 'post, Add sites that have been captured in the system',
+    notes: `Request body, an example below',
+          {    
+              "name": "xyz",
+              "county": "abcdefg"
+          }
+          `,
+  }
+},
+{
+  path: '/site',
+  method: 'PUT',
+  handler: siteController,
+  options: {
+    tags: ['api'],
+    description: 'post, Add sites that have been captured in the system using the site identification',
+    notes: `Request body, an example below',
+            {    
+                "name": "xyz", 
+                "county": 5,
+                "siteId": 51
+            }
+            `,
+  }
+}
+];
 export default route;
--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -1,7 +1,7 @@
-/* eslint-disable import/prefer-default-export */
+import { SiteDetails } from '../types/employee';
 import serviceDef from '../connection/connection';
 
-function Sites() {
+export function Sites() {
   return new Promise((resolve, reject) => {
     serviceDef.dbConnection().then((pool: any) => {
       pool.query('select * from Site', (error: any, results: any, fields: any) => {
@@ -11,4 +11,27 @@ function Sites() {
     });
   });
 }
-export default Sites;
+export function AddSites(siteDetails: SiteDetails) {
+  const { name, county } = siteDetails;
+  const sql = `Insert into Site(name, county) values ('${name}', '${county}')`;
+  return new Promise((resolve, reject) => {
+    serviceDef.dbConnection().then((pool: any) => {
+      pool.query(sql, (error: any, results: any, fields: any) => {
+        if (error) reject(error);
+        resolve(results);
+      });
+    });
+  });
+}
+export function UpdateSites(siteDetails: SiteDetails) {
+  const { name, county, siteId } = siteDetails
+  const sql = `update Site set name ='${name}', county = '${county}' where SiteID = ${siteId}`;
+  return new Promise((resolve, reject) => {
+    serviceDef.dbConnection().then((pool: any) => {
+      pool.query(sql, (error: any, results: any, fields: any) => {
+        if (error) reject(error);
+        resolve(results);
+      });
+    });
+  });
+}
